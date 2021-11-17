d3d10e368ad09e15ce54bc7d47c3bc3086142772
indonswe <afc2002afc@hotmail.com>
2021-11-17 16:08:06 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+
+import "bootstrap/dist/css/bootstrap.css"; 
+
+import DemoRouter from "./components/DemoRouter";
+
+
+
+
+
+
+ReactDOM.render(<DemoRouter />, document.getElementById("root"));
